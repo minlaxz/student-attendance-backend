2fca72299789af10b9c5721a79d7fc36892d35b2
minlaxz <mgmglatt.6991@gmail.com>
2019-12-14 23:01:18 +0630

    Firebase features updated.

--- firebase/firebase_utils.py
+++ firebase/firebase_utils.py
@@ -1,6 +1,5 @@
 from firebase_admin import db
-from datetime import date,datetime
-today=date.today()
+from datetime import datetime
 
 dayObject = datetime.now()
 day = dayObject.strftime("%d")
@@ -20,42 +19,39 @@ class Job:
 		self.ref=db.reference('project/student/'+rollNumber)
 		self.check_att=check_att
 	def register_this(self):
-		print('processing...')
+		print('utils[register]: processing to firebase...')
 		try:
 			self.ref.set({
 			u'name':self.name,
-			#u'attendance':{month : {day:'Present'}},
-			u'attendance':{month : {day: {noon : "Present"}},counter:1},
-			u'updated_date':year+'-'+month+'-'+day,
-			u'register_date_detail':str(dayObject),
-			u'register_date_short': str(x +"-"+ X),
+			u'attendance':{month : {day: {noon : dayObject.strftime("%d/%b/%Y,%X")}},counter:1},
+			u'updated_date':dayObject.strftime("%c"),
+			u'register_date_detail':dayObject.strftime("%c"),
 			u'roll':self.roll,
 			u'fingerID':self.id,
 			u'phone':self.ph
 		})
 		except Exception as e:
 			print(e)
-		print("OK. User {} added to database.".format(self.name))
+		print("utils[register]: OK. User {} added to database.".format(self.name))
 	
 	def update_this(self):
-		print('utils: processing to firebase...')
+		print('utils[update]: processing to firebase...')
 		try:
-			#e=(db.reference('project/student/'+self.roll+'/attendance').get())+1
 			e = db.reference('project/student/'+self.roll+'/attendance/'+month)
 			f = db.reference('project/student/'+self.roll+'/attendance/'+month+'/'+day+'/AM')
 			g = db.reference('project/student/'+self.roll+'/attendance/counter')
 			g.set(g.get()+1)
 			if(f.get()):
-				e.update({day: {"AM":"Present",noon:"Present"}})
+				e.update({day: {"AM":f.get(), noon:dayObject.strftime("%d/%b/%Y,%X")}})
 			else:
-				e.update({day: {noon:"Present"}})
+				e.update({day: {noon:dayObject.strftime("%d/%b/%Y,%X")}})
 			self.ref.update({
-				#u'attendance':{month : {day:'Present'}},
-				u'updated_date':year+"-"+month+"-"+day
+				u'updated_date':dayObject.strftime("%c")
 			})
 			print("utils: Done")
 		except Exception as e:
 			print(e)
+		print('utils[update]: OK. User {} updated to database'.format(self.name))
 			
 	def check_user(self):
 		if self.ref.get()==None:
