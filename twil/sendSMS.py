07280540763bcdd8bf78c6e47a8e7869228deed1
minlaxz <mgmglatt.6991@gmail.com>
2019-12-21 00:53:41 +0630

    Twilio Keys Updated.

--- twil/sendSMS.py
+++ twil/sendSMS.py
@@ -6,12 +6,12 @@ class Job:
 
     def send(self):
         from twilio.rest import Client
-        account_sid = "ACad597acdc047697847ac3d71c3bd558f"
-        auth_token = "d1475cd82a1e0ae7f5d795669ec3fef4"
+        account_sid = "AC8e7bc4b73556d29d34b061238467bc66"
+        auth_token = "58f42bc8cb3b852aff386b924d2d7aa0"
         client = Client(account_sid, auth_token)  #auth
         message = client.messages.create(
             to="+95"+str(self.ph[1:len(self.ph)]),
-            from_="+12055516681",
+            from_="+19727464285",
             body="Student: " + str(self.name)+ "," +str(self.roll) + " is under 75% attendance in this month."
         )  #msg end
         return message.sid
